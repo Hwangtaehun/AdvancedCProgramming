a316e314e7c1a210eb4cf35e16ed8b7520f4a742
skymemoryblue <113322265+skymemoryblue@users.noreply.github.com>
2022-11-14 09:05:30 +0900

    중간고사 해답

---
+++ MidtermSolution/MidtermSolution/problem1/SystemProgramming/SystemProgramming.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <Windows.h>
+#include <locale.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, NULL);
+
+    if (argc != 2) {
+        cout << "Usage: SystemProgramming 파일명" << endl;
+        return 0;
+    }
+
+    ifstream fin(argv[1], ios::in | ios::binary);
+    if (!fin) {
+        cout << argv[1] << "를 찾지 못합니다" << endl;
+        return 0;
+    }
+    const int nReads = 16;
+    char data[nReads];
+    char mem[256];
+    char disMem[nReads + 1];
+
+    memset(mem, '.', 256);
+    for (int ch = 0; ch < 256; ch++)
+    {
+        if (isprint(ch))
+            mem[ch] = ch;
+    }
+
+    size_t offset = 0;
+    while (!fin.eof())
+    {
+        fin.read(data, nReads);
+        auto len = fin.gcount();
+        
+        memset(disMem, ' ', nReads);
+
+        printf("%08llX   ", offset);
+        for (int i = 0; i < nReads; i++)
+        {
+            if (i < len)
+                printf("%02X ", data[i] & 0xff);
+            else
+                printf("   ");
+
+            disMem[i] = mem[data[i] & 0xff];
+        }
+        disMem[nReads] = NULL;
+        cout << disMem << endl;
+        offset += fin.gcount();
+    }
+    fin.close();
+    
+    return 0;
+}
---
+++ MidtermSolution/MidtermSolution/problem2/SystemProgramming/SystemProgramming.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale.h>
+#include "Base64.h"
+
+using namespace std;
+using namespace macaron;
+
+unsigned long long readLonglong(ifstream& fin)
+{
+    unsigned long long value = 0;
+    fin.read((char *) &value, sizeof(size_t));
+    return value;
+}
+
+
+string readString(ifstream& fin, bool decode = false)
+{
+    size_t len = readLonglong(fin);
+    char* buf = new char[len + 1];
+
+    fin.read((char*)buf, sizeof(char) * len);
+    buf[len] = NULL;
+
+    string input = buf;
+    delete[] buf;
+
+    string output = input;
+    if (decode) Base64::Decode(input, output);
+
+    return output;
+}
+
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, NULL);
+
+    if (argc != 2)
+    {
+        cout << "Usage: SystemProgramming 파일명" << endl;
+        return 0;
+    }
+    ifstream fin(argv[1], ios::in | ios::binary);
+
+    fin.seekg(0, ios::end);
+    auto endPos = fin.tellg();
+    fin.seekg(0, ios::beg);
+    auto pos = fin.tellg();
+
+    int nRecords = 0;
+    while (pos < endPos)
+    {
+        auto url = readString(fin, true);
+        auto phoneNum = readLonglong(fin);
+        auto desc = readString(fin);
+
+        cout << "Record[" << nRecords << "]" << endl;
+        cout << "\turl: " << url << endl;
+        cout << "\tphonenumber: " << phoneNum << endl;
+        cout << "\tdesc:" << desc << endl << endl;
+        nRecords++;
+
+        pos = fin.tellg();
+    }
+
+    fin.close();
+
+    return 0;
+}
