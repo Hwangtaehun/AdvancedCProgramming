fcde6b2fb86849c8e655525577cd626d7eea27b9
skymemoryblue <113322265+skymemoryblue@users.noreply.github.com>
2022-12-02 11:51:55 +0900

    Add files via upload

---
+++ 12월02일/main.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <iostream>
+#include <chrono> //타임관련 함수 크로노
+#include <thread>
+#include <memory>
+
+using namespace std;
+const int MaxCount = 400000;
+const int ThreadCount = 4;
+vector<int>primes;
+
+bool IsPrimeNumber(int number) {
+	if (number == 1)
+		return false;
+	if (number == 2 || number == 3)
+		return true;
+	for (int i = 2; i < number - 1; i++)
+	{
+		if ((number % i) == 0)
+			return false;
+	}
+	return true;
+}
+
+void PrintNumbers(const vector<int>& primes) {
+	for (auto prime : primes) {
+		cout << prime << " ";
+		cout << endl;
+	}
+}
+
+void func1() {
+	for (int i = 0; i < MaxCount/4; i++)
+	{
+		//if (i % 4 == 0)
+			if (IsPrimeNumber(i));
+				//primes.push_back(i);
+	}
+}
+
+void func2() {
+	for (int i = MaxCount/4; i < MaxCount/2; i++)
+	{
+		//if (i % 4 == 1)
+			if (IsPrimeNumber(i));
+				//primes.push_back(i);
+	}
+}
+
+void func3() {
+	for (int i = MaxCount/2; i < MaxCount*3/4; i++)
+	{
+		//if (i % 4 == 2)
+			if (IsPrimeNumber(i));
+				//primes.push_back(i);
+	}
+}
+
+void func4() {
+	for (int i = MaxCount * 3 / 4; i < MaxCount; i++)
+	{
+		//if (i % 4 == 3)
+			if (IsPrimeNumber(i));
+				//cout << i << endl;
+				//primes.push_back(i);
+	}
+}
+
+void main() {
+	
+	//vector<shared_ptr<thread>>threads;
+	auto start = chrono::system_clock::now();
+
+	thread t1(func1);
+	thread t2(func2);
+	thread t3(func3);
+	thread t4(func4);
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+
+	auto end = chrono::system_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+	cout << "Took " << duration << " milliseconds." << endl;
+	//PrintNumbers(primes);
+}
